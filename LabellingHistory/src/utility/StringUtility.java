0e30d8b94424a32d4317a315dfa8727b130efb69
mithul.bhansali <mithul.bhansali@N9522.intellectdesign.com>
2017-03-09 22:47:21 +0530

    StringUtility - Name Refractoring - String removeLastCharacter(StringBuilder sb)

--- LabellingHistory/src/utility/StringUtility.java
+++ LabellingHistory/src/utility/StringUtility.java
@@ -2,19 +2,27 @@ package utility;
 
 public class StringUtility {
 
-	public static String getLastPartAfterDelimiter(String string, char delimiter) {
-		int delimiterLastIndex = string.lastIndexOf(delimiter);
-		return (delimiterLastIndex == -1) ? null : string.substring(delimiterLastIndex+1);
+	public static String getLastPartAfterDelimiter(String str, char delimiter) {
+		int delimiterLastIndex = str.lastIndexOf(delimiter);
+		return (delimiterLastIndex == -1) ? null : str.substring(delimiterLastIndex+1);
 	}
 	
-	public static String getLastPartAfterDelimiter(String string, String delimiter) {
-		int delimiterLastIndex = string.lastIndexOf(delimiter);
-		return (delimiterLastIndex == -1) ? null : string.substring(delimiterLastIndex+1);
+	public static String getLastPartAfterDelimiter(String str, String delimiter) {
+		int delimiterLastIndex = str.lastIndexOf(delimiter);
+		return (delimiterLastIndex == -1) ? null : str.substring(delimiterLastIndex+1);
 	}
 	
-	public static String removeLastPartAfterDelimiter(String string, String delimiter) {
-		int delimiterLastIndex = string.lastIndexOf(delimiter);
-		return (delimiterLastIndex == -1) ? string : string.substring(0, delimiterLastIndex);
+	public static String removeLastPartAfterDelimiter(String str, String delimiter) {
+		int delimiterLastIndex = str.lastIndexOf(delimiter);
+		return (delimiterLastIndex == -1) ? str : str.substring(0, delimiterLastIndex);
+	}
+	
+	public static String removeLastCharacter(String str) {
+		return str.substring(0, str.length() - 1);
+	}
+	
+	public static String removeLastCharacter(StringBuilder sb) {
+		return sb.substring(0, sb.length() - 1);
 	}
 	
 	public static void main(String[] args) {
