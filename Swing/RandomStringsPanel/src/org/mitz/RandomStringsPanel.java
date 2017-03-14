6e3b9d881e8286ee4e2d1cc39bce887bad9bcf3c
mithul.bhansali <mithul.bhansali80@gmail.com>
2017-03-14 21:13:26 +0530

    RandomStringsPanel
    Stage 3: String centered w.r.t. to its container.

--- Swing/RandomStringsPanel/src/org/mitz/RandomStringsPanel.java
+++ Swing/RandomStringsPanel/src/org/mitz/RandomStringsPanel.java
@@ -6,6 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class RandomStringsPanel extends JPanel{
+	
+	private final static int CHARACTER_HEIGHT_PIXEL = 8;
+	private final static int CHARACTER_WIDTH_PIXEL = 6;
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("String panel");
@@ -22,6 +25,12 @@ public class RandomStringsPanel extends JPanel{
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawString("Hello World", 10, 20);
+		int width = getWidth();
+		int height = getHeight();
+		String str = "Hello World";
+		int strX = (width / 2) - (str.length() * CHARACTER_WIDTH_PIXEL / 2);
+		int strY = (height / 2) + CHARACTER_HEIGHT_PIXEL / 2;
+
+		g.drawString("Hello World", strX, strY);
 	}
 }
