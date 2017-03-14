ded09fb822b0207015539f540c6ba1968cc400bd
mithul.bhansali <mithul.bhansali80@gmail.com>
2017-03-14 21:07:43 +0530

    RandomStringsPanel
    Stage 2: String on Panel

--- Swing/RandomStringsPanel/src/org/mitz/RandomStringsPanel.java
+++ Swing/RandomStringsPanel/src/org/mitz/RandomStringsPanel.java
@@ -1,20 +1,27 @@
 package org.mitz;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class RandomStringsPanel {
+public class RandomStringsPanel extends JPanel{
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("String panel");
 		
-		JPanel drawingArea = new JPanel();
+		JPanel drawingArea = new RandomStringsPanel();
 		
 		window.setContentPane(drawingArea);
 		window.setSize(500, 300);
 		window.setLocation(20, 20);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);;
+		window.setVisible(true);
 	}
 	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawString("Hello World", 10, 20);
+	}
 }
